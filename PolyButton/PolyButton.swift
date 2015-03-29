27badc19dd7a1a8d93b3371406b3996c53eb8426
Benjamin Hass <bjhass@gmail.com>
2015-03-28 22:07:39 -0700

    Initial Commit

---
+++ PolyButton/PolyButton.swift
@@ -0,0 +1,59 @@
+//
+//  PolyButton.swift
+//  Triangles
+//
+
+import UIKit
+/*
+A polygonal UIButton that responds to touch events correctly within its bounds.
+Credit to http://www.planet1107.net/blog/custom-shape-uibutton-tutorial/ for the idea.
+*/
+class PolyButton: UIButton {
+    
+    /// The path outlining the button.
+    var bezierPath: UIBezierPath = UIBezierPath()
+    
+    init(points: [CGPoint], color: UIColor, frame: CGRect) {
+        super.init()
+        
+        if points.count < 3 {
+            println("Cannot create a polygonal button with less than three points.")
+            return
+        }
+        self.frame = frame
+        self.backgroundColor = color
+        
+        // Draw the path moving from point to point
+        bezierPath.moveToPoint(points[0])
+        for var index = 1; index < points.count; ++index {
+            bezierPath.addLineToPoint(points[index])
+        }
+        bezierPath.closePath()
+        
+        // Apply the mask over the button
+        var mask = CAShapeLayer()
+        mask.frame = self.bounds
+        mask.path = bezierPath.CGPath
+        self.layer.mask = mask
+        self.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    func buttonAction(sender: UIButton) {
+        println("Button pressed")
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func hitTest(point: CGPoint, withEvent event: UIEvent?) -> UIView? {
+        if bezierPath.containsPoint(point) {
+            return self
+        }
+        return nil
+    }
+}
---
+++ PolyButton/SampleButtonsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  PolyButton
+//
+//  Created by Benjamin Hass on 3/27/15.
+//  Copyright (c) 2015 Benjamin Hass. All rights reserved.
+//
+
+import UIKit
+
+class SampleButtonsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var p1 = CGPoint(x: 200, y: 50.0)
+        var p2 = CGPoint(x: 300, y: 150.0)
+        var p3 = CGPoint(x: 100, y: 150.0)
+        
+        var triangle = PolyButton(points: [p1,p2,p3,], color: UIColor.greenColor(), frame: self.view.bounds)
+        
+        var p4 = CGPoint(x: 150.0, y: 200.0)
+        var p5 = CGPoint(x: 250.0, y: 200.0)
+        var p6 = CGPoint(x: 250.0, y: 300.0)
+        var p7 = CGPoint(x: 150.0, y: 300.0)
+        
+        var square = PolyButton(points: [p4,p5,p6,p7], color: UIColor.redColor(), frame: self.view.bounds)
+        
+        var p8 = CGPoint(x: 200.0, y: 350.0)
+        var p9 = CGPoint(x: 105.0, y: 419.0)
+        var p10 = CGPoint(x: 141.0, y: 531.0)
+        var p11 = CGPoint(x: 259.0, y: 531.0)
+        var p12 = CGPoint(x: 295.0, y: 419.0)
+        
+        var pentagon = PolyButton(points: [p8,p9,p10,p11,p12], color: UIColor.blueColor(), frame: self.view.bounds)
+        
+        self.view.addSubview(triangle)
+        self.view.addSubview(square)
+        self.view.addSubview(pentagon)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
